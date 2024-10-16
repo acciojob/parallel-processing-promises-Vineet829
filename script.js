5f5a0a67cd21066b43e3485f615027806a3dfcbf
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-16 16:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
+document.addEventListener("DOMContentLoaded", () => {
+  const output = document.getElementById("output");
+  const btn = document.getElementById("download-images-button");
 
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
+  const images = [
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/238/200/300" },
+    { url: "https://picsum.photos/id/239/200/300" },
+  ];
 
-function downloadImages(imageArray) {
-  const fetchedPromises = imageArray.map((image) => {
-    return fetch(image.url)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Failed to load image's URL: ${image.url}`);
-        }
-        return image.url;
-      });
-  });
-
-  Promise.all(fetchedPromises)
-    .then((urls) => {
-      urls.forEach((url) => {
-        const imgElement = document.createElement("img");
-        imgElement.src = url;
-        output.appendChild(imgElement);
-      });
-    })
-    .catch((err) => {
-      console.error(err);
-      output.innerHTML = `<p>Error: ${err.message}</p>`;
+  function downloadImages(imageArray) {
+    const fetchedPromises = imageArray.map((image) => {
+      return fetch(image.url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Failed to load image's URL: ${image.url}`);
+          }
+          return image.url;
+        });
     });
-}
 
-btn.addEventListener("click", () => {
-  downloadImages(images);
+    Promise.all(fetchedPromises)
+      .then((urls) => {
+        urls.forEach((url) => {
+          const imgElement = document.createElement("img");
+          imgElement.src = url;
+          output.appendChild(imgElement);
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        output.innerHTML = `<p>Error: ${err.message}</p>`;
+      });
+  }
+
+  btn.addEventListener("click", () => {
+    downloadImages(images);
+  });
 });
+
